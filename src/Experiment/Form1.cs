598b687b5bf8a705e2cadd796000a053e1708fe0
MrVitaly <Vitaly.email@mail.ru>
2018-11-24 17:04:43 +0300

    Add CefSharp to the project.

--- src/Experiment/Form1.cs
+++ src/Experiment/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace Experiment
 {
   public partial class Form1 : Form
   {
+    public ChromiumWebBrowser cromiumWebBrowser;
     public Form1()
     {
       InitializeComponent();
+      InitializeCromium();
+    }
+
+    private void InitializeCromium()
+    {
+      CefSettings cefSettings = new CefSettings();
+      Cef.Initialize(cefSettings);
+      cromiumWebBrowser = new ChromiumWebBrowser("http://opensource.spotify.com/cefbuilds/index.html");
+      this.Controls.Add(cromiumWebBrowser);
+      cromiumWebBrowser.Dock = DockStyle.Fill;
+    }
+
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+    {
+
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      Cef.Shutdown();
     }
   }
 }
